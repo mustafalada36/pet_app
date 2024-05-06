c582e55498f607477bb1aa6619a1fce6cb1cf02c
Muhammad Mustafa <116518684+mustafalada36@users.noreply.github.com>
2024-05-07 03:17:28 +0500

    update

--- lib/Screens/signup.dart
+++ lib/Screens/signup.dart
@@ -9,6 +9,7 @@ class SignUpScreen extends StatefulWidget {
 
 class _SignUpScreenState extends State<SignUpScreen> {
   bool _acceptTerms = false;
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
@@ -20,42 +21,67 @@ class _SignUpScreenState extends State<SignUpScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(
-                'assets/your_image.png',
-                height: 150,
+              Container(
+                width: 360.0,
+                height: 214.0,
+                child: Image.asset('assets/images/Group8.png',
+                    width: 360.0, height: 214.0),
               ),
               const SizedBox(height: 20.0),
-              IconButton(
-                icon: const Icon(Icons.verified_user),
-                onPressed: () {
-                  // Add functionality for icon button
-                },
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 1.0),
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                padding: const EdgeInsets.all(8.0),
+                child:
+                    const Icon(Icons.person, size: 40.0, color: Colors.black),
               ),
-              SizedBox(height: 20.0),
-              Text(
+              const SizedBox(height: 10.0),
+              const Text('Create Account',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+              const SizedBox(height: 20.0),
+              const Text(
                 'Welcome! Please enter your information below and get started.',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 20.0),
+              const SizedBox(height: 20.0),
               TextFormField(
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   labelText: 'Email',
-                  border: OutlineInputBorder(),
+                  border: InputBorder.none,
+                  filled: true,
+                  fillColor: Colors.white,
                 ),
               ),
-              SizedBox(height: 10.0),
+              const SizedBox(height: 10.0),
               TextFormField(
-                obscureText: true,
+                obscureText: _obscureText,
                 decoration: InputDecoration(
                   labelText: 'Password',
-                  border: OutlineInputBorder(),
+                  border: InputBorder.none,
+                  filled: true,
+                  fillColor: Colors.white,
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
                 ),
               ),
-              SizedBox(height: 20.0),
+              const SizedBox(height: 20.0),
               Row(
                 children: [
                   Checkbox(
@@ -66,15 +92,43 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       });
                     },
                   ),
-                  Text('Accept Terms and Conditions'),
+                  const Text('Accept Terms and Conditions'),
                 ],
               ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Add functionality for create account button
-                },
-                child: Text('Create Account'),
+              const SizedBox(height: 20.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Add functionality for create account button
+                      },
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(14.0),
+                          ),
+                        ),
+                      ),
+                      child: const Text('Create Account'),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Already have an account?'),
+                  // SizedBox(width: 2),
+                  TextButton(
+                    onPressed: () {
+                      // Add functionality for log in button
+                    },
+                    child: const Text('Log in here!'),
+                  ),
+                ],
               ),
             ],
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pet_app/Screens/signup.dart';
 import 'package:pet_app/Screens/splashscreen.dart';
 
 void main() async {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: const SignUpScreen(),
     );
   }
 }
