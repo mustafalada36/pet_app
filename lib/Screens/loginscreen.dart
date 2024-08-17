631065a9580e97cf3da2a62294b540b9900d38c5
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-08-17 14:17:23 +0500

    changes

--- lib/Screens/loginscreen.dart
+++ lib/Screens/loginscreen.dart
@@ -9,65 +9,113 @@ class LoginScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 40),
-              Center(
-                child: Image.asset('assets/images/Group1386.png'),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: GoogleFonts.montserrat(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 40),
+                Center(
+                 child: Image.asset('assets/images/loginimg.png',width: 360.0, height: 214.0),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: GoogleFonts.montserrat(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15,
+                      color: Colors.black,
+                    ),
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  style: GoogleFonts.montserrat(
                     fontWeight: FontWeight.w500,
                     fontSize: 15,
                     color: Colors.black,
                   ),
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: GoogleFonts.montserrat(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15,
                       color: Colors.black,
                     ),
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                      ),
+                    ),
                   ),
-                ),
-                style: GoogleFonts.montserrat(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 15,
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: GoogleFonts.montserrat(
+                  style: GoogleFonts.montserrat(
                     fontWeight: FontWeight.w500,
                     fontSize: 15,
                     color: Colors.black,
                   ),
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.black,
+                ),
+                SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {
+                      // Forgot password action
+                    },
+                    child: Text(
+                      'Forgot password?',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ),
-                style: GoogleFonts.montserrat(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 15,
-                  color: Colors.black,
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const OTPScreen()),
+                          );
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xFF267E1E)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(14.0),
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w500, // medium weight
+                            color: Colors.white, // text color
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 20),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {
-                    // Forgot password action
-                  },
+                SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.centerRight,
                   child: Text(
-                    'Forgot password?',
+                    "You don’t have an account?",
                     style: GoogleFonts.montserrat(
                       fontWeight: FontWeight.w400,
                       fontSize: 12,
@@ -75,117 +123,72 @@ class LoginScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const OTPScreen()),
-                        );
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            const Color(0xFF267E1E)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(14.0),
+                SizedBox(height: 70),
+
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton.icon(
+                        icon: Image.asset(
+                          'assets/images/google-logo-9822.png',
+                          width: 17,
+                          height: 17,
+                        ),
+                        label: Text(
+                          'Continue with Google',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400, // regular weight
+                            color: Color(0xFF999999), // text color
+                          ),
+                        ),
+                        onPressed: () {
+                          // Continue with Google action
+                        },
+                        style: ButtonStyle(
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                            EdgeInsets.symmetric(vertical: 12, horizontal: 20),
                           ),
                         ),
                       ),
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w500, // medium weight
-                          color: Colors.white, // text color
-                        ),
-                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  "You don’t have an account?",
-                  style: GoogleFonts.montserrat(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 12,
-                    color: Colors.black,
-                  ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton.icon(
-                      icon: Image.asset(
-                        'assets/images/google-logo-9822.png',
-                        width: 17,
-                        height: 17,
-                      ),
-                      label: Text(
-                        'Continue with Google',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w400, // regular weight
-                          color: Color(0xFF999999), // text color
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton.icon(
+                        icon: Image.asset(
+                          'assets/images/logo-facebookpng-32202.png',
+                          width: 17,
+                          height: 17,
                         ),
-                      ),
-                      onPressed: () {
-                        // Continue with Google action
-                      },
-                      style: ButtonStyle(
-                        padding: MaterialStateProperty.all<EdgeInsets>(
-                          EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                        label: Text(
+                          'Continue with Facebook',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400, // regular weight
+                            color: Colors.white, // text color
+                          ),
                         ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton.icon(
-                      icon: Image.asset(
-                        'assets/images/logo-facebookpng-32202.png',
-                        width: 17,
-                        height: 17,
-                      ),
-                      label: Text(
-                        'Continue with Facebook',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w400, // regular weight
-                          color: Colors.white, // text color
-                        ),
-                      ),
-                      onPressed: () {
-                        // Continue with Facebook action
-                      },
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Color(0xFF3B5998)),
-                        padding: MaterialStateProperty.all<EdgeInsets>(
-                          EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+                        onPressed: () {
+                          // Continue with Facebook action
+                        },
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Color(0xFF3B5998)),
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                            EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-            ],
+                  ],
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
