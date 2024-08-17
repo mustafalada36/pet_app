1f9969c357cd6e56344b93bc4fec0bf288755589
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-08-17 13:17:02 +0500

    push

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -1,3 +1,17 @@
 
 
+import 'package:flutter/cupertino.dart';
 
+class homeScreen extends StatefulWidget {
+  const homeScreen({super.key});
+
+  @override
+  State<homeScreen> createState() => _homeScreenState();
+}
+
+class _homeScreenState extends State<homeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
