1ef5021731275fde34d82880d9b53686b6c79bfd
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-10-01 17:30:36 +0500

    home

---
+++ lib/Reuseable Components/customTextField.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class customTextField extends StatelessWidget {
+  final double width;
+  final double height;
+  final String hintText;
+  final TextStyle hintStyle;
+
+  customTextField({
+    required this.width,
+    required this.height,
+    this.hintText = '',
+    TextStyle? hintStyle, // Accept hintStyle as optional
+  }) : hintStyle = hintStyle ??
+            TextStyle(
+              // Set a default hintStyle if none is provided
+              color: Colors.grey, // Default hint text color
+              fontSize: 16, // Default hint text size
+            );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: hintStyle,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(22.0),
+            borderSide: BorderSide(
+              color: Color(0xFF267E1E), // Green border color
+              width: 2.0,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(
+              color: Color(0xFF267E1E),
+              width: 2.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
