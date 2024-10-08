b3f0faa332f32c0a00441870daf1bb0917b6dccf
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-10-08 09:52:58 +0500

    Update customTextField.dart

--- lib/Reuseable Components/customTextField.dart
+++ lib/Reuseable Components/customTextField.dart
@@ -5,18 +5,21 @@ class customTextField extends StatelessWidget {
   final double height;
   final String hintText;
   final TextStyle hintStyle;
+  final BorderRadius borderRadius;
 
   customTextField({
     required this.width,
     required this.height,
     this.hintText = '',
-    TextStyle? hintStyle, // Accept hintStyle as optional
-  }) : hintStyle = hintStyle ??
+    TextStyle? hintStyle,
+    BorderRadius? borderRadius, // Accept hintStyle as optional
+  })  : hintStyle = hintStyle ??
             TextStyle(
               // Set a default hintStyle if none is provided
               color: Colors.grey, // Default hint text color
               fontSize: 16, // Default hint text size
-            );
+            ),
+        borderRadius = borderRadius ?? BorderRadius.circular(22);
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +31,7 @@ class customTextField extends StatelessWidget {
           hintText: hintText,
           hintStyle: hintStyle,
           enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(22.0),
+            borderRadius: borderRadius,
             borderSide: BorderSide(
               color: Color(0xFF267E1E), // Green border color
               width: 2.0,
