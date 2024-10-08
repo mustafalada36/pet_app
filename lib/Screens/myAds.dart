d3c09e439c70268c46d1b8ff8356f6af4d6310a2
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-10-08 09:52:52 +0500

    Create myAds.dart

---
+++ lib/Screens/myAds.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:pet_app/Reuseable%20Components/topActiveContainer.dart';
+import 'package:pet_app/Screens/favourites.dart';
+import 'package:pet_app/constants.dart';
+
+class myAds extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "My Ads",
+            style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w900,
+                color: primaryColor),
+          )),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              topActiveContainer("Ads", 120),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: CircleAvatar(
+                  backgroundColor: primaryColor,
+                  child: Text(
+                    "1",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 15),
+          adsCard(),
+        ],
+      ),
+    );
+  }
+}
