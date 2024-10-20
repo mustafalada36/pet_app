3508b246de6b8cf2be46a7ef322331144c93aaab
Muhammad Mustafa <mustafalada36@gmail.com>
2024-10-21 01:38:55 +0500

    Buy Screen

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ import 'package:pet_app/Screens/splashscreen.dart';
 // import 'package:pet_app/Screens/testingScreen.dart';
 
 import 'Screens/addNewAddress.dart';
+import 'Screens/buyScreen.dart';
 
 void main() async {
   // WidgetsFlutterBinding.ensureInitialized();
@@ -40,7 +41,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF267E1E)),
         useMaterial3: true,
       ),
-      home: pets(),
+      home: CustomScreen(),
 
       //shows
       // debugShowCheckedModeBanner: false,
