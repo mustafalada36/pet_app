c1b8e7502581f6365dac6e4cd0fd257d3c328fe2
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-10-22 23:46:32 +0500

    home

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF267E1E)),
         useMaterial3: true,
       ),
-      home: addNewAddress(),
+      home: buyFood(),
 
       //shows
       // debugShowCheckedModeBanner: false,
