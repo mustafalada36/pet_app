5c73151dd09eb8e588ae5e6a7e9476da31215267
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-10-23 00:43:17 +0500

    home

---
+++ lib/Reuseable Components/primaryButton.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../Screens/homeScreen.dart';
+
+class primaryButton extends StatelessWidget {
+  final double width;
+  final String text;
+
+  primaryButton(this.width, this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const homeScreen()),
+          );
+        },
+        style: ButtonStyle(
+          backgroundColor:
+              WidgetStateProperty.all<Color>(const Color(0xFF267E1E)),
+          shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(14.0),
+            ),
+          ),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+            // medium weight
+            color: Colors.white, // text color
+          ),
+        ),
+      ),
+    );
+  }
+}
