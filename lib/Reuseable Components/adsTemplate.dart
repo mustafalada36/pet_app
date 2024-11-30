a7bf36fa8bc4220c2dddfbc14b8d6092c4c3a473
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-12-01 01:05:17 +0500

    successful ads showing

--- lib/Reuseable Components/adsTemplate.dart
+++ lib/Reuseable Components/adsTemplate.dart
@@ -1,57 +1,76 @@
 import 'package:flutter/material.dart';
-
 import '../constants.dart';
 
 class adsTemplate extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final String breed;
+  final String location;
+  final String price;
+
+  adsTemplate({
+    required this.imageUrl,
+    required this.name,
+    required this.breed,
+    required this.location,
+    required this.price,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 180,
+      height: 300,
       child: Card(
         color: secondaryColor,
         elevation: 0,
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
                 padding: const EdgeInsets.only(top: 10, bottom: 0),
-                child: Image.asset("assets/images/rabbitss.png"),
+                child: Image.network(
+                  imageUrl,
+                  width: 150,
+                  height: 150,
+                  fit: BoxFit.cover,
+                ),
               ),
               const SizedBox(height: 10),
-              const Text(
-                "Bella",
-                style: TextStyle(
+              Text(
+                name,
+                style: const TextStyle(
                     color: Colors.black,
                     fontSize: 16,
                     fontWeight: FontWeight.w700),
               ),
-              const Padding(
-                padding: EdgeInsets.only(left: 5, top: 5),
+              Padding(
+                padding: const EdgeInsets.only(left: 5, top: 5),
                 child: Text(
-                  "Rabbit Breed",
-                  style: TextStyle(
+                  breed,
+                  style: const TextStyle(
                     color: Colors.black,
                     fontSize: 14,
                   ),
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.only(left: 5, top: 5),
+              Padding(
+                padding: const EdgeInsets.only(left: 5, top: 5),
                 child: Text(
-                  "Johar Town",
-                  style: TextStyle(
+                  location,
+                  style: const TextStyle(
                     color: Colors.black,
                     fontSize: 14,
                   ),
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.only(left: 5, top: 5),
+              Padding(
+                padding: const EdgeInsets.only(left: 5, top: 5),
                 child: Text(
-                  "PKR 20,000",
-                  style: TextStyle(
+                  "PKR $price",
+                  style: const TextStyle(
                       color: Colors.black,
                       fontSize: 16,
                       fontWeight: FontWeight.w700),
